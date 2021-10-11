2741d5bfed3d6531ab7340ab21df34db881232f1
Romain Lafosse <romain75l@gmail.com>
2021-10-11 19:54:58 +0200

    Picture, title and text in Home added / react-icons imported.

--- candles-front/src/components/Header/index.js
+++ candles-front/src/components/Header/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { BiShoppingBag } from 'react-icons/bi';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__logo">
-        <img src="" alt="logo" />
+        <img src="" alt="LOGO" />
         <NavLink className="header__link" to="#">Bougies</NavLink>
         <NavLink className="header__link" to="#">Mon Histoire</NavLink>
       </div>
       <div className="header__user">
         <NavLink className="header__user__link" to="#">Mon Compte</NavLink>
-        <img className="header__user__img" src="" alt="panier"/>
+        <BiShoppingBag />
       </div>
     </header>
   );
--- candles-front/src/components/Home/index.js
+++ candles-front/src/components/Home/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import CandleImg from '../../assets/img/photo0602.jpg';
 
 const Home = () => {
   return (
-    <div>je suis dans home.</div>
+    <div className="home">
+      <div className="home__img">
+        <img className="home__img__picture" src={CandleImg} alt="bougie" />
+      </div>
+      <div className="home__collection">je suis la collection de bougies.
+      </div>
+      <div className="home__story">
+        <h2 className="home__story__title">Mon Histoire</h2>
+        <p className="home__story__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas sed quidem culpa. Consectetur consequuntur, iure porro sunt ducimus voluptatum, doloribus perspiciatis rem illo asperiores recusandae. Aperiam sequi magnam accusamus sapiente!
+        Voluptates, commodi consectetur modi doloremqe adipisci veritatis natus eos a facilis, aliquam, possimus voluptatibus. Unde iste dignissimos temporibus illo, obcaecati aspernatur repudiandae expedita, porro, nobis officia quibusdam dolor at nostrum.
+        Ipsam laudantium deserunt officia earum labore corporis, dolores rerum sed incidunt est, saepe iure sunt. Aut dolorem doloribus saepe delectus impedit repellat facilis numquam sunt doloremque natus labore, deserunt aliquam.
+        Totam odio cupiditate nesciunt id rem ipsa? Suscipit explicabo, vel esse sint veritatis, asperiores nemo fugit iste quae, cumque blanditiis temporibus in delectus enim est. Fugit, numquam? Vitae, eius praesentium.
+        Corporis tempore, atque, ab illo architecto labore suscipit maiores dolores id aliquid deserunt earum soluta natus dolore! Corporis laborum, officiis, culpa debitis inventore recusandae consectetur dicta, impedit dolor ipsum quidem.
+        Quisquam, eius vel at aperiam nihil numquam quas necessitatibus facere laboriosam consequatur cum possimus consequuntur deserunt reprehenderit accusamus! Quaerat dignissimos vero beatae ipsam aliquam ad cum amet. Sed, voluptatum culpa.
+        Eaque possimus quas repellendus aspernatur porro illo, laboriosam expedita temporibus voluptatum at vitae praesentium voluptate ad sed sint? Aliquid quisquam perspiciatis sequi dignissimos dolorum. Asperiores incidunt quam atque mollitia nobis.
+        Magnam officiis aspernatur architecto! Velit excepturi, magnam rerum quidem aperiam ullam repudiandae quisquam? Ullam, ut provident deleniti nostrum, tenetur beatae, atque nisi eligendi fugit excepturi inventore modi et sint. Modi.
+        Dolore aliquid, animi modi, saepe vitae, ea quo rerum provident maiores quis delectus! Voluptate aliquam ut architecto esse est, laboriosam molestias accusantium quod doloribus atque sed beatae similique modi illo.
+        Maiores ad quibusdam, aliquid consectetur quas delectus accusamus! Eaque minima commodi libero velit! Perspiciatis, sapiente doloribus earum culpa sint adipisci non quibusdam tempora expedita provident itaque aspernatur necessitatibus cumque quidem.
+        Aut necessitatibus delectus quaerat velit rem, voluptatum quod optio ipsa! Quam labore culpa voluptatem magnam nihil in, quos, pariatur voluptatibus aut distinctio maiores perspiciatis soluta hic amet, dolores doloribus repudiandae!
+        Quibusdam hic facilis tenetur non aut assumenda unde blanditiis perspiciatis, temporibus omnis doloremque veniam earum dicta ullam odio sapiente itaque sed fugit quidem laboriosam est explicabo iure, commodi ducimus. Expedita.
+        Ratione earum laudantium sit facere, nihil, quibusdam, optio perferendis at provident placeat distinctio quia. Ipsam temporibus saepe autem! Rerum eius assumenda aliquid itaque maxime ab aut quaerat architecto tempore molestias.
+        </p>
+      </div>
+    </div>
   );
 }
 
